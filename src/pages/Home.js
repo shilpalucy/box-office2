88b6dbf1eae0daa9b1ada0820df97aee179799b2
shilpalucy <shilpa.lucy@gmail.com>
2023-01-09 06:29:03 +0300

    created search box

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React,{useState} from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
+
 const Home = () => {
+
+
+  
+  const[Input,setInput]=useState("");
+
+  const onSearch=()=>{
+    fetch(`https://api.tvmaze.com/search/shows?q=${Input}`)
+    .then(r=>r.json())
+    .then(result=> console.log(result));
+  }
+
+  const onKeyDown=(ev)=>{
+    if(ev.keyCode===13)
+      onSearch();
+  }
+
+  const onEventChange = (ev) => {
+    setInput(ev.target.value);
+  
+  }
   return (
+
     <MainPageLayout>
-      This is home page.I want to check contents
+      <input type="text" onChange = {onEventChange} onKeyDown={onKeyDown} value={Input}/>
+      <button type="button" onClick={onSearch}>Search</button>
     </MainPageLayout>
   );
 }
