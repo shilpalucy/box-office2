921f79075470b6d45d8bda08a56b184a6aa23cfd
shilpalucy <shilpa.lucy@gmail.com>
2023-01-12 15:14:13 +0300

    show page is created using useRender method

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,38 +1,66 @@
 //eslint-disable-next-line 
-import React,{useEffect, useState} from 'react';
+import React,{useEffect, useReducer} from 'react';
 import {useParams} from 'react-router-dom';
 import { apiGet } from '../misc/config';
 
+const reducer=(prev,action)=>{
+    switch(action.type){
+    case "FETCH_SUCCESS": {
+        return {
+        isLoading:false,
+        error:null,
+        show:action.show
+    }
+
+    }
+    case "FETCH_FAILED":{
+        return {
+            isLoading:false,
+            error:action.error,
+        }   
+
+    }
+    default :
+        return prev;
+}
+}
+const initialState={
+    show : null,
+    isLoading: true,
+    error: null
+
+}
 const Show = () => {
     const { id } = useParams();
-    const [show, setShow] = useState(null);
-    const [isLoading,setIsLoading]=useState(true);
-    const [error,setError]=useState(null);
+    
+    const [state, dispatch] = useReducer(reducer, initialState);
 
+    //const [show, setShow] = useState(null);
+    //const [isLoading,setIsLoading]=useState(true);
+    //const [error,setError]=useState(null);
 
+    console.log(state);
     useEffect(() => {
         
         let isMounted=true;
         apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(result => {setTimeout(()=> {
             if(isMounted){
-          setShow(result);
-          setIsLoading(false);
+          dispatch({type:'FETCH_SUCCESS',show:result});
             }
         },2000)})
         .catch(
             (err => {
                 if (isMounted){
-                setError(err.message);
-                setIsLoading(false);
+                    dispatch({type:'FETCH_FAILED',error:err.message});
                 }
         }
     ));
     return ()=>{isMounted=false};
     }, [id]);
-if(isLoading){
+if(state.isLoading){
  return <div>Data is Loading</div>
 }
-if(error){
+if(state.error){
     return <div>error occured</div>
 }   
  return null;   
