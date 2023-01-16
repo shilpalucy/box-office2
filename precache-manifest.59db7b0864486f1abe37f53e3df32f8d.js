891f9679f5f779b3c5da25b01cc2de7097358284
shilpalucy <shilpa.lucy@gmail.com>
2023-01-16 08:07:07 +0300

    Updates

---
+++ precache-manifest.59db7b0864486f1abe37f53e3df32f8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c206b4ec00320e745f1a8eca52168ba",
+    "url": "/box-office2/index.html"
+  },
+  {
+    "revision": "a088eb51b3b5ed0ad151",
+    "url": "/box-office2/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "31cb9510ddd0c4df68e8",
+    "url": "/box-office2/static/js/2.a0cb6758.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/box-office2/static/js/2.a0cb6758.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a088eb51b3b5ed0ad151",
+    "url": "/box-office2/static/js/main.7306705f.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/box-office2/static/js/main.7306705f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c62cb9e4c95a78537b1b",
+    "url": "/box-office2/static/js/runtime-main.002450dc.js"
+  }
+]);
